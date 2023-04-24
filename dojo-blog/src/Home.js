0ad77d3a085409b8e189ea9383754d1600bf654d
Roberto Torres <roberto.torres.dev@gmail.com>
2023-04-24 19:29:13 -0400

    using click

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,20 @@
 const Home = () => {
+
+    const handleClick = (e)=>{
+        alert("hello!")
+        console.log(e)
+    }
+
+    const handleSecondClick = (name, e)=>{
+        alert("Hello, " + name)
+        console.log(e)
+    }
+
     return ( 
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={handleClick}>Click me!</button>
+            <button onClick={(e) => handleSecondClick("Sococo", e)}>Click me!</button>
         </div>
      );
 
