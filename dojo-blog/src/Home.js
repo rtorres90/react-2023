1b7993f87846371b8354e10e3b6bcfe289da235c
Roberto Torres <roberto.torres.dev@gmail.com>
2023-04-25 22:43:21 -0400

    using states

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,4 +1,7 @@
+import {useState} from 'react';
+
 const Home = () => {
+    const [status, setStatus] = useState('Sad');
 
     const handleClick = (e)=>{
         alert("hello!")
@@ -10,11 +13,17 @@ const Home = () => {
         console.log(e)
     }
 
+    const handleThirdClick = () => {
+        setStatus("Happy!")
+    }
+
     return ( 
         <div className="home">
             <h2>Home Page</h2>
             <button onClick={handleClick}>Click me!</button>
             <button onClick={(e) => handleSecondClick("Sococo", e)}>Click me!</button>
+            <h1>{ status }</h1>
+            <button onClick={handleThirdClick}>Change State!</button>
         </div>
      );
 
